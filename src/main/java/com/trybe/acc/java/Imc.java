bb444438925f17505a39605e7e994553a83e187e
johnny <johnnyrubischreiner@gmail.com>
2022-07-26 22:46:10 -0300

    exercicio concluido

--- src/main/java/com/trybe/acc/java/Imc.java
+++ src/main/java/com/trybe/acc/java/Imc.java
@@ -4,23 +4,25 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Imc {
+  /**
+   * class main
+   */
+  public static void main(String[] args) {
+    // Ler a entrada de um número pelo console
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-	public static void main(String[] args) {
-			// Ler a entrada de um número pelo console	
-			Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-			
-			System.out.println("Digite o peso:");
-			float peso = scan.nextFloat();
-			
-			System.out.println("Digite a aultura:");
-			float altura = scan.nextFloat();
-			
-			// Seu código aqui
-			
-					
-			// Imprime a saída no console. OBS: descomente a próxima linha
-			//System.out.println(imc);
+    System.out.println("Digite o peso:");
+    float peso = scan.nextFloat();
 
-	}
+    System.out.println("Digite a aultura:");
+    float altura = scan.nextFloat();
+
+    // Seu código aqui
+    float imc = peso / (altura * altura);
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println(imc);
+
+  }
 
 }
